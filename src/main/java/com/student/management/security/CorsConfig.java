7155bec255784460d448a4077f40b9a05f9e6b90
kranthi-kumar-2004 <159746543+kranthi-kumar-2004@users.noreply.github.com>
2026-04-14 13:11:12 +0530

    Update CorsConfig.java

--- src/main/java/com/student/management/security/CorsConfig.java
+++ src/main/java/com/student/management/security/CorsConfig.java
@@ -1,3 +1,10 @@
+package com.student.management.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @Configuration
 public class CorsConfig {
 
@@ -11,7 +18,6 @@ public class CorsConfig {
                         .allowedOriginPatterns("https://ecomerce-front-phi.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
-                        .exposedHeaders("Authorization")
                         .allowCredentials(true)
                         .maxAge(3600);
             }
