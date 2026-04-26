5b8d130b9f741871ceb45f8a84489351ee94df75
kranthi-kumar-2004 <159746543+kranthi-kumar-2004@users.noreply.github.com>
2026-04-26 11:10:27 +0530

    Update LoginRequest.java

--- src/main/java/com/student/management/dto/LoginRequest.java
+++ src/main/java/com/student/management/dto/LoginRequest.java
@@ -2,15 +2,15 @@ package com.student.management.dto;
 
 public class LoginRequest {
 
-    private String username;
+    private String email;
     private String password;
 
-    public String getUsername() {
-        return username;
+    public String getEmail() {
+        return email;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
+    public void setEmail(String username) {
+        this.email = username;
     }
 
     public String getPassword() {
